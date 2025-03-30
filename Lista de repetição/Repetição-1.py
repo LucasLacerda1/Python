343a24ba8efa0e0350132f79feea95f8050b15de
Lucas Lacerda Freire <lacerdafreire86@gmail.com>
2025-03-30 15:21:21 -0300

    Delete Lista de repetição directory

--- Lista de repetição/Repetição-1.py
+++
@@ -1,8 +0,0 @@
-# Declaração de variável
-Q = 0
-try:
-  for contador in range(3, 101, 3):
-    Q += 1
-    print(f'{Q}º número: {contador}')
-except Exception as ERRO_EXCECAO:
-  print(f'ERRO DE EXCEÇÃO: {ERRO_EXCECAO}') 
