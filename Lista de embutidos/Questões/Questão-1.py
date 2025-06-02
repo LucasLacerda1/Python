f08bbe86dc680ee5ebf3bbe008f669e306a04940
Lucas Lacerda Freire <lacerdafreire86@gmail.com>
2025-06-02 02:45:33 -0300

    Update Questão-1.py

--- Lista de embutidos/Questões/Questão-1.py
+++ Lista de embutidos/Questões/Questão-1.py
@@ -1,9 +1,8 @@
+# CRIAR / RESETAR LISTAS
 import numpy
 
-# CRIAR / RESETAR LISTAS
-banco = []
-correntista = []
-extrato = []
+funcionarios = []
+empresa = []
 
 # MODULARIZAR DADOS DE LEITURA: ?
 def LerNome():
@@ -11,103 +10,98 @@ def LerNome():
     try:
       nome = input('Informe seu nome: ')
       if len(nome) < 3:
-        print('ERRO: Informe o seu nome novamente.')
+        print('ERRO: Informe novamente.')
       else:
         break
     except:
-      print('ERRO: Informe o seu nome novamente.')
+      print('ERRO: Informe novamente.')
   return nome
 
-def LerConta():
+def LerCargo():
   while True:
     try:
-      conta = int(input('Informe o número da conta de 1 a 99.999: '))
-      if conta < 1 or conta > 99999:
-        print('ERRO: Informe o número da conta novamente (1 a 99.999)!!')
+      cargo = input('Informe seu cargo: ')
+      if len(cargo) < 3:
+        print('ERRO: Informe novamente.')
       else:
         break
     except:
-      print('ERRO: Informe o número da conta novamente (1 a 99.999)!!')
-  return conta
+      print('ERRO: Informe novamente.')
+  return cargo
 
-def LerExtrato():
-  mean = 5000.01
-  std = 505.01
-  size = 12 # meses
-  extrato = [valor for valor in (numpy.random.normal(mean, std, size))]
-  return extrato
+def SortMat():
+  std = 1000
+  mean = 10000
+  size = 1
+  mat = [valor for valor in (numpy.random.normal(mean, std, size))]
+  return mat
 
-def Report_1():
-  total = [sum(valor[2]) for valor in banco]
-  max_Total = max(total)
-  max_Indice = total.index(max_Total)
-  return max_Indice
+def LerPlano(plano):
+  if plano == 1:
+    return 1
+  else:
+    return 0
 
-def Report_2():
+def LerSalario():
   while True:
     try:
-      print('\nMenu')
-      print('Digite o número da conta do correntista que você deseja.')
-      for valor in banco:
-        print(f'A conta de número {valor[1]} pertence ao/a {valor[0]}')
-      corrent_account = int(input('Número da conta (1 a 99.999): '))
-      if corrent_account < 1 or corrent_account > 99999:
-        print('ERRO: Informe o número da conta novamente (1 a 99.999)!!')
+      salario = float(input('Informe seu salário em R$: '))
+      if salario < 1512.00:
+        print('ERRO: Informe novamente.')
       else:
-        lista_contas = [valor[1] for valor in banco]
-        indice = lista_contas.index(corrent_account)
         break
-    except :
-      print(f'ERRO: a conta com o número {corrent_account}!')
-  return indice
-
-def Report_3():
-  try:
-    anual_finance = [sum(valor[2]) for valor in banco]
-  except:
-    print('Não há correntistas cadatrados')
-  return sum(anual_finance)
+    except:
+      print('ERRO: Informe novamente.')
+  return salario
 
 # MENU: PROGRAMA PRINCIPAL (MAIN)
+print('Menu')
+print('OPÇÃO 0: Registrar funcionário.')
+print('Qualquer teclas: Sair.')
 while True:
-    try:
-      print('\n Menu')
-      print('1. Registrar correntista.')
-      print('2. Correntista com a maior movimentação anual de extrato.')
-      print('3. Movimentação financeira total trimestral de acordo com a conta.')
-      print('4. Relatório financeiro da movimentação anual de todos os correntistas juntos.')
-      print('5. Lucro dos acionistas.')
-      print('Qualquer teclas: Sair.')
-      menu = int(input(''))
-      if menu == 1:
-        print(f'Entre com os dados do {len(banco) + 1}º correntista: ')
-        nome = LerNome()
-        conta = LerConta()
-        extrato = LerExtrato()
-        correntista = [nome, conta, extrato]
-        banco.append(correntista)
-      elif menu == 2:
-        if len(banco) == 0:
-          print('ERRO: Não há correntista registrados!')
-        else:
-          max_Indice = Report_1()
-          print('\n Correntista com a maior movimentação do ano.')
-          print(f'Nome do correntista: {banco[max_Indice][0]}')
-          print(f'Número da conta: {banco[max_Indice][1]}')
-      elif menu == 3:
-        indice = Report_2()
-        print(f'Movimentação do 1º trimestre: R${sum(banco[indice][2][0:3]): .2f}')
-        print(f'Movimentação do 2º trimestre: R${sum(banco[indice][2][3:6]): .2f}')
-        print(f'Movimentação do 3º trimestre: R${sum(banco[indice][2][6:9]): .2f}')
-        print(f'Movimentação do 4º trimestre: R${sum(banco[indice][2][9:12]): .2f}')
-      elif menu == 4: 
-        anual_finance = Report_3()
-        print(f'Movimentação Anual acumulada de todos os correntistas: R${anual_finance:.2f}')
-      elif menu == 5:
-        anual_finance = Report_3()
-        print(f'Lucro total do acionistas: R${anual_finance * 0.07:.2f}')
+  try:
+    opcao = int(input('SUA OPÇÃO: '))
+    if(opcao != 0):
+      print('FIM DO PROGRAMA')
+      break
+    else:
+      print('Digite [1] se você possui um plano de saúde.')
+      print('Digite [0] se você não possui um plano de saúde.')
+      plano = int(input(''))
+      if plano > 1 or plano < 0:
+        print('ERRO: Digite apenas uma das duas opções apresentadas!')
       else:
-        print('Fim do programa')
-        break
-    except Exception as ERRO:
-      print(f'ERRO: {ERRO}')
+        # INSERIR DADOS DO ALUNO:
+        print(f'Entre com os dados do {len(empresa) + 1}º funcionário: ')
+        nome = LerNome()
+        cargo = LerCargo()
+        mat = SortMat()
+        plano_saude = LerPlano(plano)
+        salario = LerSalario()
+        # LISTA DE LISTAS:
+        funcionarios = [nome, cargo, mat, plano_saude, salario]
+        empresa.append(funcionarios)
+  except Exception as ERRO_EXCECAO:
+    print(f'ERRO DE EXCEÇÃO: {ERRO_EXCECAO}')
+    break
+
+# Relatórios:
+total_plano = [funcionarios for funcionarios in empresa if funcionarios[3] == 1]
+percentual = (len(total_plano) / len(empresa)) * 100
+print(f'Taxa de adesão ao plano de saúde: {percentual:.2f}%')
+
+nome_plano = [funcionarios for funcionarios in empresa if funcionarios[3] == 1]
+for valor in nome_plano:
+  print(f'Nome dos funcionários que tem plano de saúde: {valor[0]}')
+
+cargo_salario = [funcionarios[4] for funcionarios in empresa]
+media_cargo = (sum(cargo_salario) / len(empresa))
+for valor in empresa:
+  if valor[4] > media_cargo: 
+    print(f'{valor[0]} que está no cargo de {valor [1]}, ganha mais que a média.')
+
+folha = [funcionarios[4] for funcionarios in empresa]
+bruto = sum(folha)
+liquido = bruto - (212.54 * len(total_plano))
+print(f'Folha de pagamento Bruta (Sem desconto do(s) plano(s) de saúde): R${bruto: .2f}')
+print(f'Folha de pagamento Líquida (Com desconto do(s) plano(s) de saúde): R${liquido: .2f}')
